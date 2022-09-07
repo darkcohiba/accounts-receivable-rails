592ea418caff3cba1706fb37ddf51991c91c6497
Sam <samuel.waters@gmail.com>
2022-09-07 14:00:36 -0600

    more

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -3,4 +3,10 @@ module InvoicesHelper
     return "$0.00" if cents == 0
     "$#{cents.to_s.insert(-3, ".")}"
   end
+
+
+  def to_yen(cents)
+    return "000¥" if cents == 0
+    "#{cents}¥"
+  end
 end
